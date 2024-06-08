ad42a2b3cceb092f540fac2749f8f085beb7d711
Michael Tyson <myty@users.noreply.github.com>
2024-06-08 13:22:49 +0000

    add vite

---
+++ apps/pocketbase/pb_hooks/main.pb.ts
@@ -0,0 +1,11 @@
+/// <reference path="../pb_data/types.d.ts" />
+
+// routerAdd("GET", "/hello/:name", (c) => {
+//   let name = c.pathParam("name");
+
+//   return c.json(200, { message: `Hello ${name}` });
+// });
+
+// onModelAfterUpdate((e) => {
+//   console.log("user updated...", e.model.get("email"));
+// }, "users");
